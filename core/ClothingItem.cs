0eaff42330aa5f5fea19d1d24e4176d916b5ab25
trollterminator <jacob.lageri@hotmail.com>
2025-04-07 09:48:10 +0200

    Added image property to clothing item

--- core/ClothingItem.cs
+++ core/ClothingItem.cs
@@ -6,6 +6,7 @@
         public string Size { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
         public int ClothingId { get; set; }
+        public string Image { get; set; } = string.Empty;
         public bool LentOut { get; set; } = true;
         public int OwnerId { get; set; }
 
