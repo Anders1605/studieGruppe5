983269cfcab1ba8c3b0d8eec00483aab7a1c7122
Anders <anders.otte@gmail.com>
2025-04-29 12:19:54 +0200

    Toast ved login

--- TheMarketplaceFolder/TheMarketplace/Services/UserService/UserServiceMongoDB.cs
+++ TheMarketplaceFolder/TheMarketplace/Services/UserService/UserServiceMongoDB.cs
@@ -16,24 +16,21 @@ namespace TheMarketplace.Services.UserService
             LocalStorage = localStorage;
             _client = client;
         }
+        //Bruger en user som objekt og sender den til API som opretter i Users-collection i DB
         public async Task createUser(User newUser)
         {
             await _client.PostAsJsonAsync<User>($"{serverUrl}/api/user", newUser);
         }
 
-        public bool LoggedInMockStatus()
-        {
-            throw new NotImplementedException();
-        }
-
+        //userOnline oprettes og sættes sammen med den bruger som har emailaddress og pw i db. Returnerer herefter brugeren. 
         public async Task<User> LoginMongoDB(string EmailAddress, string Password)
         {
-            User userOnline = new();
-            //Noget med https getAsync eller lignende hvor email og pw sendes til controller 
+            User userOnline = new(); 
             userOnline = await _client.GetFromJsonAsync<User>($"{serverUrl}/api/user/{EmailAddress}/{Password}/"); 
             return userOnline;
         }
 
+        //Brugeren fra localstorage sættes ind i loggedInUser. Hvis loggedInUser er null(tom) returneres false.
         public async Task<bool> CheckLogin()
         {
             User loggedInUser = new();
@@ -42,18 +39,19 @@ namespace TheMarketplace.Services.UserService
                 return false;
             else return true;
         }
-
-
+        //----------------------------------------------------
+        public bool LoggedInMockStatus()
+        {
+            throw new NotImplementedException();
+        }
         public Task SetMockUsersAsync()
         {
             throw new NotImplementedException();
         }
-
         public bool UpdateLoggedInMock(bool validation)
         {
             throw new NotImplementedException();
         }
-
         Task<bool> IUserService.Login(string EmailAddress, string Password, List<User> list)
         {
             throw new NotImplementedException();
