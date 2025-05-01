9e356717cd709945964a49d168eff0dcca13e885
Anders <anders.otte@gmail.com>
2025-05-01 08:37:20 +0200

    Sign up fix
    Added validation to user-model

--- TheMarketplaceFolder/Shared/Models/User.cs
+++ TheMarketplaceFolder/Shared/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Models
 {
@@ -9,16 +10,22 @@ namespace Shared.Models
         [BsonId]
         public ObjectId Id { get; set; }  // MongoDB will handle the _id automatically
 
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 20 characters.")]
         public string Name { get; set; }
 
-        public string TelephoneNumber { get; set; }
+        public string TelephoneNumber { get; set; } = "";
 
-        public string Address { get; set; }
+        public string Address { get; set; } = "";
 
         public string ProfilePictureUrl { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(60, MinimumLength = 10, ErrorMessage = "Name must be between 10 and 60 characters.")]
         public string EmailAddress { get; set; }
 
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Name must be between 4 and 20 characters.")]
         public string Password { get; set; }
     }
 }
